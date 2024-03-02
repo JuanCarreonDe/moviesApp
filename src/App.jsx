166e927a64cfb7db246fef5c355b701c28876920
JuanCarreonDe <juancarreondelira@gmail.com>
2024-03-02 03:57:13 -0600

    last commit

--- src/App.jsx
+++ src/App.jsx
@@ -34,19 +34,29 @@ function App() {
 
   return (
     <section className="w-full max-w-[800px] min-h-screen flex flex-col items-center gap-8 p-4">
-      <header className="flex flex-col items-center justify-center gap-4">
+      <header className="w-full flex flex-col items-center justify-center gap-4">
         <h1 className="text-2xl font-bold">Buscador de peliculas</h1>
-        <form className="flex gap-4" onSubmit={handleSubmit}>
-          <input
-            onChange={handleChange}
-            type="text"
-            value={query}
-            placeholder="La la land, Nocturnal Animals, Memento"
-            name="query"
-            className="rounded-md px-4"
-          />
-          <input type="checkbox" onChange={handleSort} checked={sort} />
-          <button>Search</button>
+        <form className="w-full flex flex-col gap-4" onSubmit={handleSubmit}>
+          <div className="flex gap-4 justify-center items-center">
+            <input
+              onChange={handleChange}
+              type="text"
+              value={query}
+              placeholder="La la land, Nocturnal Animals, Memento"
+              name="query"
+              className="rounded-md px-4 py-2"
+            />
+            <button>Search</button>
+          </div>
+          <div className="flex gap-2 justify-end">
+            <input
+              type="checkbox"
+              name="sort"
+              onChange={handleSort}
+              checked={sort}
+            />
+            <label htmlFor="sort">Orden alfabético</label>
+          </div>
         </form>
         {error && <span className="text-red-600">{error}</span>}
       </header>
